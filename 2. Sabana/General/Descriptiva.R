473f98fc81ea3eeba08e4cb4883b6d40d7aa68c7
andresromeroparra <anropar.economista@gmail.com>
2021-10-31 14:23:45 -0500

    Registro Administrativo

--- 2. Sabana/General/Descriptiva.R
+++
@@ -1,7 +0,0 @@
-# Descriptiva
-
-print(dim(DATA))
-View(head(DATA))
-print(names(DATA))
-view(dfSummary(as.data.frame(DATA)))
-View(head(DATA))
--- 2. Sabana/General/Nombres de campos.R
+++
@@ -1,9 +0,0 @@
-#Nombres de campos
-
-#Esta en DATA pero no se encuentra en el diccionario.
-Error_Nombres_variables_1=setdiff(names(DATA), c("A01","IdIntegrante",Campos))#Identifica las variables que están en la base de datos pero no en el diccionario.
-
-#Esta en el diccionario pero no se encuentra DATA.
-Error_Nombres_variables_2=setdiff(c("A01","IdIntegrante",Campos), names(DATA))#Identifica las variables que están en el diccionario pero no están en la base de datos.
-
-rm(list = ls()[ls() %in% grep("^capitulo|^[V|v]ar",ls(),value = TRUE)])
